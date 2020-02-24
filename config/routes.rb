374ddf5f9a00a50938df3323f77bb8640ea69783
Shoriii <cerezos1206@gmail.com>
2020-02-24 09:40:31 +0000

    Finish[2-4]

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ Rails.application.routes.draw do
   root 'home#top'
   get 'home/about' => 'home#about'
   devise_for :users
-  resources :users,only: [:show,:index,:edit,:update]
+  resources :users,only: [:show,:index,:edit,:update] do
+     member do
+     get :following, :followers
+     end
+  end
+  resources :relationships,only: [:create, :destroy]
   resources :books
-
 end
